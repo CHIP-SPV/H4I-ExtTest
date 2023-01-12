808ac20dceb291800a51fe1508cc964645c3fba7
Philip C. Roth <rothpc@ornl.gov>
2023-01-12 19:39:53 +0000

    Add license info.

--- CMake/ExtTestVersion.cmake
+++ CMake/ExtTestVersion.cmake
@@ -1,1 +1,3 @@
+# Copyright 2021-2023 UT-Battelle
+# See LICENSE.txt in the root of the source distribution for license info.
 set(ExtTest_VERSION "0.0.0")
--- src/HipBLAS/Sgemm/Common/HipblasContext.h
+++ src/HipBLAS/Sgemm/Common/HipblasContext.h
@@ -1,4 +1,4 @@
-// Copyright 2021 UT-Battelle
+// Copyright 2021-2023 UT-Battelle
 // See LICENSE.txt in the root of the source distribution for license info.
 #ifndef TEST_HIPBLAS_CONTEXT_H
 #define TEST_HIPBLAS_CONTEXT_H
